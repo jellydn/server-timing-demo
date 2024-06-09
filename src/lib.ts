2cbc381df440cba9d8c36c6204ddef003062e57b
Huynh Duc Dung <dunghd.it@gmail.com>
2024-06-09 19:53:08 +0800

    feat(env-var): change import style and enable TS extension import
    Apply automatic changes
    
    feat(lib.ts): refactor token retrieval and usage

--- src/lib.ts
+++ src/lib.ts
@@ -2,18 +2,20 @@ import env from "env-var";
 
 const GITHUB_URL = "https://api.github.com";
 
-export async function getGithubUser(username?: string) {
-	const response = await fetch(`${GITHUB_URL}/users/${username}`, {
-		method: "GET",
-		headers: env.get("GITHUB_TOKEN")
-			? {
-					Authorization: `token ${env.get("GITHUB_TOKEN").asString()}`,
-				}
-			: {},
-	});
-	if (response.ok) {
-		return response.json();
-	}
+const token = env.get("GITHUB_TOKEN");
 
-	throw new Error(response.statusText);
+export async function getGithubUser(username?: string) {
+  const response = await fetch(`${GITHUB_URL}/users/${username}`, {
+    method: "GET",
+    headers: token
+      ? {
+        Authorization: `token ${token.asString()}`,
+      }
+      : {},
+  });
+  if (response.ok) {
+    return response.json();
+  }
+
+  throw new Error(response.statusText);
 }
