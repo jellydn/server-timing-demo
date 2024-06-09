a1465ac42c584ac85c3c7bcb375e982201dfb33c
jellydn <jellydn@users.noreply.github.com>
2024-06-09 09:00:15 +0000

    Apply automatic changes

--- src/lib.ts
+++ src/lib.ts
@@ -3,17 +3,17 @@ import env from "env-var";
 const GITHUB_URL = "https://api.github.com";
 
 export async function getGithubUser(username?: string) {
-  const response = await fetch(`${GITHUB_URL}/users/${username}`, {
-    method: "GET",
-    headers: env.get("GITHUB_TOKEN")
-      ? {
-        Authorization: `token ${env.get("GITHUB_TOKEN").asString()}`,
-      }
-      : {},
-  });
-  if (response.ok) {
-    return response.json();
-  }
+	const response = await fetch(`${GITHUB_URL}/users/${username}`, {
+		method: "GET",
+		headers: env.get("GITHUB_TOKEN")
+			? {
+					Authorization: `token ${env.get("GITHUB_TOKEN").asString()}`,
+				}
+			: {},
+	});
+	if (response.ok) {
+		return response.json();
+	}
 
-  throw new Error(response.statusText);
+	throw new Error(response.statusText);
 }
