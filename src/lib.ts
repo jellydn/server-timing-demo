842d5e2a919bef11ac8518892933069cb6571ce8
Huynh Duc Dung <dunghd.it@gmail.com>
2024-06-09 16:59:47 +0800

    feat: Add env-var package and use it for environment variable access

--- src/lib.ts
+++ src/lib.ts
@@ -1,17 +1,19 @@
+import env from "env-var";
+
 const GITHUB_URL = "https://api.github.com";
 
 export async function getGithubUser(username?: string) {
-	const response = await fetch(`${GITHUB_URL}/users/${username}`, {
-		method: "GET",
-		headers: process.env.GITHUB_TOKEN
-			? {
-					Authorization: `token ${process.env.GITHUB_TOKEN}`,
-				}
-			: {},
-	});
-	if (response.ok) {
-		return response.json();
-	}
+  const response = await fetch(`${GITHUB_URL}/users/${username}`, {
+    method: "GET",
+    headers: env.get("GITHUB_TOKEN")
+      ? {
+        Authorization: `token ${env.get("GITHUB_TOKEN").asString()}`,
+      }
+      : {},
+  });
+  if (response.ok) {
+    return response.json();
+  }
 
-	throw new Error(response.statusText);
+  throw new Error(response.statusText);
 }
